55d7c78ebb193d129d472a84295e5976581d4f82
simple <nguiom@unal.edu.co>
2023-11-04 03:15:34 -0500

    cansado

--- Lab_3/hello_turtle/scripts/keyboard.py
+++ Lab_3/hello_turtle/scripts/keyboard.py
@@ -5,19 +5,19 @@ import sys
 from khbit import KBHit
 
 class ReadKey(self):
-	def __init__(self): 
-		rospy.init_node('Keyboard', anonymous=False)
-		self.key =rospy.Publisher('key',str, queue_size=10)
-		rate = rospy.Rate(10)
+    def __init__(self): 
+	rospy.init_node('Keyboard', anonymous=False)
+	self.key =rospy.Publisher('key',str, queue_size=10)
+	rate = rospy.Rate(10)
 		
-        	while not rospy.is_shutdown():
-			self.getKey()
-			rate.sleep()
+        while not rospy.is_shutdown():
+	    self.getKey()
+	    rate.sleep()
 		
-	def getKey(self):
-		if(KBHit.kbhit()):
-			self.last_key=KBHit.getch()
-			self.key.publish(self.last_key)
+    def getKey(self):
+	if(KBHit.kbhit()):
+	    self.last_key=KBHit.getch()
+	    self.key.publish(self.last_key)
 
 if __name__== "__main__": 
     try:
--- Lab_3/hello_turtle/scripts/teleport.py
+++ Lab_3/hello_turtle/scripts/teleport.py
@@ -6,33 +6,33 @@ from turtlesim.msg import Pose
 from turtlesim.srv import TeleportAbsolute Teleport
 
 class Teleport(self):
-	def __init__(self): 
-		rospy.init_node('Teleport', anonymous=False)
-		self.key_sub= rospy.Subscriber('key',Pose, self.update_key)
-		self.key=0
-		self.tabla={' ':(1),'r':(2)}
-		while not rospy.is_shutdown():
-			rospy.spin()
-			if(self.tabla==1):
-				teleportA()
-			elif(self.tabla==2):
-				teleportR()
-	def teleportA(self):
-		rospy.wait_for_service('/turtle1/teleport_absolute')
-		try:
-        		teleportA = rospy.ServiceProxy('/turtle1/teleport_absolute', TeleportAbsolute)
-        		resp1 = teleportA(5, 5, 0)
-   		 except rospy.ServiceException as e:
-        		print(str(e))
-	def teleportR(self):
-		rospy.wait_for_service('/turtle1/teleport_relative')
-		try:
-        		teleportA = rospy.ServiceProxy('/turtle1/teleport_absolute', TeleportAbsolute)
-        		resp1 = teleportR(0,3.1416)
-   		 except rospy.ServiceException as e:
-        		print(str(e))
-	def update_key(self,data):
-		self.key=self.tabla.get(data,(0))
+    def __init__(self): 
+	rospy.init_node('Teleport', anonymous=False)
+	self.key_sub= rospy.Subscriber('key',Pose, self.update_key)
+	self.key=0
+	self.tabla={' ':(1),'r':(2)}
+	while not rospy.is_shutdown():
+	    rospy.spin()
+	    if(self.tabla==1):
+		teleportA()
+	    elif(self.tabla==2):
+		teleportR()
+    def teleportA(self):
+	rospy.wait_for_service('/turtle1/teleport_absolute')
+	try:
+            teleportA = rospy.ServiceProxy('/turtle1/teleport_absolute', TeleportAbsolute)
+            resp1 = teleportA(5, 5, 0)
+   	except rospy.ServiceException as e:
+            print(str(e))
+    def teleportR(self):
+	rospy.wait_for_service('/turtle1/teleport_relative')
+	try:
+            teleportA = rospy.ServiceProxy('/turtle1/teleport_absolute', TeleportAbsolute)
+            resp1 = teleportR(0,3.1416)
+   	except rospy.ServiceException as e:
+            print(str(e))
+    def update_key(self,data):
+	self.key=self.tabla.get(data,(0))
 
 if __name__== "__main__": 
     try:
