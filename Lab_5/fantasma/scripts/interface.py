25b552b242b3520f35b163cb2a38c14c202383e2
simple <nguiom@unal.edu.co>
2023-11-28 21:12:56 -0500

    codigo

---
+++ Lab_5/fantasma/scripts/interface.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Int32MultiArray
+from tkinter import *
+from tkinter import ttk
+import numpy as np
+from dynamixel_workbench_msgs.srv import DynamixelCommand
+
+class interface():
+    def __init__(self,root):
+
+        rospy.init_node('State', anonymous=False)
+        self.key_sub= rospy.Subscriber("/dynamixel_workbench/joint_states",JointState, self.read)
+        self.move =rospy.Publisher('move',Int32MultiArray, queue_size=10)
+        rate=rospy.Rate(0.5)
+
+        self.state=0
+        root.title("Lineal")
+        self.goals={}
+
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+        
+        self.z=0
+        self.zOff = StringVar()
+        self.zOff.set(136)
+        feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.zOff)
+        feet_entry.grid(column=3, row=1, sticky=(W, E))
+        ttk.Label(mainframe, text="Offset").grid(column=1, row=1, sticky=(W, E))
+        ttk.Button(mainframe, text="Save", command=self.offset).grid(column=5, row=1, sticky=W)
+        ttk.Button(mainframe, text="Grab", command=self.grab).grid(column=4, row=1, sticky=W)
+
+        self.xC=StringVar()
+        self.yC=StringVar()
+        self.zC=StringVar()
+        ttk.Label(mainframe, text="Current").grid(column=1, row=2, sticky=(W, E))
+        ttk.Label(mainframe, textvariable=self.xC).grid(column=2, row=2, sticky=(W, E))
+        ttk.Label(mainframe, textvariable=self.yC).grid(column=3, row=2, sticky=(W, E))
+        ttk.Label(mainframe, textvariable=self.zC).grid(column=4, row=2, sticky=(W, E))
+        ttk.Button(mainframe, text="On/off", command=self.on).grid(column=5, row=2, sticky=W)
+
+        self.xG=StringVar()
+        self.xG.set(100)
+        self.yG=StringVar()
+        self.yG.set(100)
+        self.name=StringVar()
+        self.name.set("p1")
+        self.nameList=[]
+        ttk.Label(mainframe, text="Goal").grid(column=1, row=3, sticky=(W, E))
+        xG_entry = ttk.Entry(mainframe, width=7, textvariable=self.xG)
+        xG_entry.grid(column=2, row=3, sticky=(W, E))
+        yG_entry = ttk.Entry(mainframe, width=7, textvariable=self.yG)
+        yG_entry.grid(column=3, row=3, sticky=(W, E))
+        name_entry = ttk.Entry(mainframe, width=7, textvariable=self.name)
+        name_entry.grid(column=4, row=3, sticky=(W, E))
+        ttk.Button(mainframe, text="Save", command=self.newGoal).grid(column=5, row=3, sticky=W)
+
+        self.p1=StringVar()
+        self.p2=StringVar()
+        self.p1_entry=ttk.Combobox(mainframe, textvariable=self.p1,postcommand=self.updateP1)
+        self.p1_entry.grid(column=1, row=4, sticky=(W, E))
+        self.p2_entry=ttk.Combobox(mainframe, textvariable=self.p2,postcommand=self.updateP2)
+        self.p2_entry.grid(column=3, row=4, sticky=(W, E))
+        ttk.Button(mainframe, text="Go", command=self.go).grid(column=5, row=5, sticky=W)
+
+        for child in mainframe.winfo_children(): 
+            child.grid_configure(padx=5, pady=5)
+
+        
+
+    def offset(self):
+        try:
+            self.z = int(self.zOff.get())
+        except ValueError:
+            pass
+
+    def on(self):
+        rospy.wait_for_service('dynamixel_workbench/dynamixel_command')
+        try:
+            dynamixel_command = rospy.ServiceProxy(
+                '/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+            dynamixel_command('',1,'Torque_Enable',self.state)
+            dynamixel_command('',2,'Torque_Enable',self.state)
+            dynamixel_command('',3,'Torque_Enable',self.state)
+            dynamixel_command('',4,'Torque_Enable',self.state)
+            dynamixel_command('',5,'Torque_Enable',self.state)
+            if(self.state==1):
+                self.state=0
+            elif(self==0):
+                self.state=1
+        except rospy.ServiceException as exc:
+            print(str(exc))
+
+    def grab(self):
+        rospy.wait_for_service('dynamixel_workbench/dynamixel_command')
+        try:
+            dynamixel_command = rospy.ServiceProxy(
+                '/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+            dynamixel_command('',5,'Goal_Position',187)
+        except rospy.ServiceException as exc:
+            print(str(exc))
+
+    def newGoal(self,*args):
+        self.goals.update({self.name.get():[int(float(self.xG.get())),\
+        int(self.yG.get()),self.z]})
+        self.nameList.append(self.name.get())
+
+    def updateP1(self,*args):
+        self.p1_entry['values']=self.nameList
+
+    def updateP2(self,*args):
+        self.p2_entry['values']=self.nameList
+    
+    def go(self):
+        temp1=self.p1.get()
+        temp2=self.p2.get()
+        temp1=self.goals.get(temp1,[100,1,136])
+        temp2=self.goals.get(temp2,[100,1,136])
+        message=Int32MultiArray(data=temp1+temp2)
+        rospy.loginfo(message)
+        self.move.publish(message)
+    
+    def read(self,data):
+        pass
+
+if __name__== "__main__": 
+        root = Tk()
+        test=interface(root)
+        root.mainloop()
---
+++ Lab_5/fantasma/scripts/move.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int32MultiArray
+from dynamixel_workbench_msgs.srv import DynamixelCommand
+import numpy as np
+
+class Moving(): 
+	def __init__(self): 
+		rospy.init_node('Moving', anonymous=False)
+		self.goal_serve = rospy.Subscriber('move',Int32MultiArray, self.judge)
+		self.goal=[500,500,500,500]
+		self.jointCommand('',1,'Goal_Position',self.goal[0], 0.5)
+		self.jointCommand('',2,'Goal_Position',self.goal[1], 0.5)
+		self.jointCommand('',3,'Goal_Position',self.goal[2], 0.5)
+		self.jointCommand('',4,'Goal_Position',self.goal[3], 0.5)
+		rospy.spin()
+
+	def jointCommand(self,command, id_num, addr_name, value, time):
+		rospy.wait_for_service('dynamixel_workbench/dynamixel_command')
+		try:        
+			dynamixel_command = rospy.ServiceProxy(
+            	'/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+			result = dynamixel_command(command,id_num,addr_name,value)
+			return result.comm_result
+		except rospy.ServiceException as exc:
+				print(str(exc))
+
+	def judge(self,msg):
+		data=msg.data
+		pos1=np.array([data[0],data[1],data[2]])
+		pos2=np.array([data[3],data[4],data[5]])
+		q1,q2,q3,q4=self.makeLinea(pos1,pos2)
+		for i in range(51):
+			self.goal[0]=int(np.interp(q1[i]*(180/np.pi)+150,[0,300],[0,1022]))
+			self.goal[1]=int(np.interp(q2[i]*(180/np.pi)+150,[0,300],[0,1022]))
+			self.goal[2]=int(np.interp(q3[i]*(180/np.pi)+150,[0,300],[0,1022]))
+			self.goal[3]=int(np.interp(q4[i]*(180/np.pi)+150,[0,300],[0,1022]))
+			rospy.loginfo([self.goal[0],self.goal[1],self.goal[2],self.goal[3]])
+			self.jointCommand('',1,'Goal_Position',self.goal[0], 0.5)
+			self.jointCommand('',2,'Goal_Position',self.goal[1], 0.5)
+			self.jointCommand('',3,'Goal_Position',self.goal[2], 0.5)
+			self.jointCommand('',4,'Goal_Position',self.goal[3], 0.5)
+		
+
+	def equation(self,x,y,z):
+		q1=np.arctan2(y,x)
+		a=np.sqrt(x**2+y**2)
+		alp=np.arctan2(z-136,a)
+		b=np.sqrt((z-136)**2+a**2)
+		q3s=np.sqrt(1-((b**2-2*(106**2))/(2*(106**2)))**2)
+		q3c=(b**2-2*(106**2))/(2*(106**2))
+		q3=np.arctan2(q3s,q3c)
+		bets=(106*np.sin(q3+np.pi))/b
+		betc=np.sqrt(1-((106*np.sin(q3+np.pi))/b)**2)
+		bet=np.arctan2(bets,betc)
+		q2=alp+bet-np.pi/2
+		rotc=((1.0*(-1.0*np.sin(q2 + 1.5707963267949))*np.sin(q3) + (1.0*np.cos(q2 + 1.5707963267949))*np.cos(q3)))/\
+		(1.0*(1.0*(-1.0*np.sin(q2 + 1.5707963267949))*np.cos(q3) - (1.0**np.cos(q2 + 1.5707963267949))*np.sin(q3)))
+		rots=1/(1.0*(-1.0*np.sin(q3)*np.sin(q2 + 1.5707963267949) + 1.0*np.cos(q3)*np.cos(q2 + 1.5707963267949))*rotc - \
+		(1.0*np.sin(q3)*np.cos(q2 + 1.5707963267949) + 1.0*np.sin(q2 + 1.5707963267949)*np.cos(q3)))
+		rotc=rotc*rots
+		q4=np.arctan2(rots,rotc)
+		return (q1,q2,q3,q4)
+
+	def linea(self,p1,p2):
+		x=np.zeros([1,21])
+		y=np.zeros([1,21])
+		z=np.zeros([1,21])
+		vn=np.linalg.norm(p2-p1)
+		for i in range(51):
+			x[0,i]=p1[0]+((i*vn)/20)*((p2[0]-p1[0])/vn)
+			y[0,i]=p1[1]+((i*vn)/20)*((p2[1]-p1[1])/vn)
+			z[0,i]=p1[2]+((i*vn)/20)*((p2[2]-p1[2])/vn)
+		return x,y,z
+
+	def makeLinea(self,p1,p2):
+		x,y,z=self.linea(p1,p2)
+		q1=np.empty((1,21))
+		q2=np.empty((1,21))
+		q3=np.empty((1,21))
+		q4=np.empty((1,21))
+		for i in range(51):
+			q1[0,i],q2[0,i],q3[0,i],q4[0,i]=self.equation(x[0,i],y[0,i],z[0,i])
+			#move goal
+		q1=q1.flat
+		q2=q2.flat
+		q3=q3.flat
+		q4=q4.flat
+		return q1,q2,q3,q4
+
+if __name__== "__main__": 
+	try:
+		temp=Moving()
+	except: 
+		rospy.loginfo("End of the trip for Robotica 2023-2") 
